86ab993dcd3686872fe0cd2107a5ccf93ba4db12
Soo <soojlee0106@naver.com>
2023-04-17 00:34:09 +0900

    change username into input format

--- lang.py
+++ lang.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-username = "montycoder0701"
+username = input("Enter your username: ")
 url = f"https://github.com/{username}?tab=repositories"
 response = requests.get(url)
 soup = BeautifulSoup(response.content, "html.parser")
