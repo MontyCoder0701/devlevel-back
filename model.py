73425f8ca0daef056fb6f02d8e3d8669adee53f5
Soo <soojlee0106@naver.com>
2023-04-17 02:32:07 +0900

    Analyze contributions

--- model.py
+++ model.py
@@ -19,3 +19,6 @@ repo_soup = new_soup(repo_url)
 
 default_url = new_url(username, "default")
 default_soup = new_soup(default_url)
+
+backend_lang = ["C#", "C++", "C", "Go", "Java", "PHP", "Python", "Ruby", "Rust", "Perl"]
+frontend_lang = ["CSS", "HTML", "JavaScript", "TypeScript", "Kotlin", "Swift"]
--- views.py
+++ views.py
@@ -10,12 +10,51 @@ def get_contributions_count() -> int:
     contributions_count = contributions_h2.get_text(strip=True).split()[0].replace(",", "")
     return int(contributions_count)
 
-
 def get_years_active() -> int:
     buttons = default_soup.find_all("a", class_="js-year-link")
     count = 0
     for button in buttons:
         if f"/{username}?tab=overview" in button["href"]:
             count += 1
-            
     return count
+
+def analyze_languages() -> str:
+    languages = get_top_languages()
+    if len(languages) == 0:
+        return "Not enough languages to analyze"
+    else:
+        backend_count = 0
+        frontend_count = 0
+        for language in languages:
+            if language in backend_lang:
+                backend_count += 1
+            if language in frontend_lang:
+                frontend_count += 1
+
+        if abs(backend_count - frontend_count) < 3:
+            return "Fullstack"
+        else:
+            if backend_count > frontend_count:
+                return "Backend"
+            if frontend_count > backend_count:
+                return "Frontend"
+            
+
+def analyze_contributions() -> str:
+    contributions = get_contributions_count()
+    if contributions <= 100:
+        return "Low activity"
+    elif contributions <= 300:
+        return "Medium activity"
+    else:
+        return "High activity"
+ 
+def analyze_years_active() -> str:
+    years_active = get_years_active()
+    if years_active <= 1:
+        return "Newbie"
+    elif years_active <= 3:
+        return "Junior"
+    else:
+        return "Senior"
+    
