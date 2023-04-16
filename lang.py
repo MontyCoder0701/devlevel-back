581dff6b21c9d0987730b93d994bd9c9b215cbec
Soo <soojlee0106@naver.com>
2023-04-17 01:11:29 +0900

    get years and contributions

--- lang.py
+++ lang.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-username = input("Enter your username: ")
+username = input("Enter your username (case sensitive): ")
 url = f"https://github.com/{username}?tab=repositories"
 response = requests.get(url)
 soup = BeautifulSoup(response.content, "html.parser")
@@ -9,4 +9,4 @@ soup = BeautifulSoup(response.content, "html.parser")
 elements = soup.select("span[itemprop='programmingLanguage']")
 texts = [element.text for element in elements]
 
-print(texts)
+print(f"List of top languages: {texts}")
