e3dbdb4a2dca7b8688811ee6ce312ed8a9eee965
Soo <soojlee0106@naver.com>
2023-04-17 09:50:52 +0900

    Jsonify result

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from model import new_soup, new_url
 from views import *
 
@@ -23,11 +23,13 @@ def api():
     contributions = get_contributions_count(default_soup)
     years_active = get_years_active(default_soup, username)
 
-    return {
+    response = {
         'languages': analyze_languages(languages),
         'contributions': analyze_contributions(contributions),
         'years_active': analyze_years_active(years_active)
     }
+
+    return jsonify(response)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000', debug=True)
