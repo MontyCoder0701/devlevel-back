410170757421d389b322c1e8b38231020fa4d581
Soo <soojlee0106@naver.com>
2023-04-17 09:32:28 +0900

    Add backend lang

--- model.py
+++ model.py
@@ -20,5 +20,5 @@ def new_soup(url: str) -> BeautifulSoup:
     return soup
 
 
-backend_lang = ["C#", "C++", "C", "Go", "Java", "PHP", "Python", "Ruby", "Rust", "Perl", "Jupyter Notebook"]
+backend_lang = ["C#", "C++", "C", "Go", "Java", "PHP", "Python", "Ruby", "Rust", "Perl", "Jupyter Notebook", "Shell", "Scala", "Dockerfile"]
 frontend_lang = ["CSS", "HTML", "JavaScript", "TypeScript", "Kotlin", "Swift"]
